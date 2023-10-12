2ba7b7a78d3b13953a4d18aae34e74556cce98bc
TheAsel <a.osele@proton.me>
2023-10-12 12:33:27 +0200

    improved api trycatch, added table skeleton

--- src/pages/EncounterPage.vue
+++ src/pages/EncounterPage.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { requestCreatures, requestFilters } from '../utils/api-calls';
+import { filtersStore, creaturesStore } from 'stores/store';
+import SkeletonTable from 'src/components/Encounter/SkeletonTable.vue';
 import CreaturesTable from 'src/components/Encounter/CreaturesTable.vue';
 import CreatureList from 'src/components/Encounter/CreatureList.vue';
+
+const filters = filtersStore();
+const creatures = creaturesStore();
+const ready = ref(false);
+
+// ---- API requests
+try {
+  if (filters.getFilters.family.length === 0) {
+    filters.updateFamilies(await requestFilters('families'));
+  }
+
+  if (filters.getFilters.alignment.length === 0) {
+    filters.updateAlignments(await requestFilters('alignments'));
+  }
+
+  if (filters.getFilters.size.length === 0) {
+    filters.updateSizes(await requestFilters('sizes'));
+  }
+
+  if (filters.getFilters.rarity.length === 0) {
+    filters.updateRarities(await requestFilters('rarities'));
+  }
+
+  if (creatures.getCreatures.length === 0) {
+    creatures.updateCreatures(await requestCreatures(0, -1));
+  }
+  ready.value = true;
+} catch (error) {
+  console.log(error);
+}
 </script>
 
 <template>
   <q-page class="tw-flex row items-center justify-evenly">
-    <CreaturesTable />
+    <CreaturesTable v-if="ready" />
+    <SkeletonTable v-else />
     <CreatureList />
   </q-page>
 </template>
--- src/utils/api-calls.ts
+++ src/utils/api-calls.ts
@@ -1,63 +1,51 @@
 import { backendUrl } from 'src/boot/globals';
 
 export async function requestCreatures(start: number, end: number) {
-  try {
-    const requestOptions = {
-      method: 'GET',
-      headers: { accept: 'application/json' }
-    };
-    const response = await fetch(
-      backendUrl +
-        '/bestiary/list?sort_key=Name&order_by=Ascending&cursor=' +
-        start +
-        '&page_size=' +
-        end,
-      requestOptions
-    );
-    const data = await response.json();
-    if (!response.ok) {
-      const error = (data && data.message) || response.status;
-      throw error;
-    }
-    return data.results;
-  } catch (error) {
-    console.log(error);
+  const requestOptions = {
+    method: 'GET',
+    headers: { accept: 'application/json' }
+  };
+  const response = await fetch(
+    backendUrl +
+      '/bestiary/list?sort_key=Name&order_by=Ascending&cursor=' +
+      start +
+      '&page_size=' +
+      end,
+    requestOptions
+  );
+  const data = await response.json();
+  if (!response.ok) {
+    const error = (data && data.message) || response.status;
+    throw error;
   }
+  return data.results;
 }
 
 export async function requestFilters(filter: 'families' | 'alignments' | 'sizes' | 'rarities') {
-  try {
-    const requestOptions = {
-      method: 'GET',
-      headers: { accept: 'application/json' }
-    };
-    const response = await fetch(backendUrl + '/bestiary/' + filter, requestOptions);
-    const data = await response.json();
-    if (!response.ok) {
-      const error = (data && data.message) || response.status;
-      throw error;
-    }
-    return data;
-  } catch (error) {
-    console.log(error);
+  const requestOptions = {
+    method: 'GET',
+    headers: { accept: 'application/json' }
+  };
+  const response = await fetch(backendUrl + '/bestiary/' + filter, requestOptions);
+  const data = await response.json();
+  if (!response.ok) {
+    const error = (data && data.message) || response.status;
+    throw error;
   }
+  return data;
 }
 
 export async function encounterInfo(encounter: { enemy_levels: number[]; party_levels: number[] }) {
-  try {
-    const requestOptions = {
-      method: 'POST',
-      headers: { accept: 'application/json', 'Content-Type': 'application/json' },
-      body: JSON.stringify(encounter)
-    };
-    const response = await fetch(backendUrl + '/encounter/info', requestOptions);
-    const data = await response.json();
-    if (!response.ok) {
-      const error = (data && data.message) || response.status;
-      throw error;
-    }
-    return data;
-  } catch (error) {
-    console.log(error);
+  const requestOptions = {
+    method: 'POST',
+    headers: { accept: 'application/json', 'Content-Type': 'application/json' },
+    body: JSON.stringify(encounter)
+  };
+  const response = await fetch(backendUrl + '/encounter/info', requestOptions);
+  const data = await response.json();
+  if (!response.ok) {
+    const error = (data && data.message) || response.status;
+    throw error;
   }
+  return data;
 }
