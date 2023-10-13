5507e27479c19e2375b8dcb753b1c2bd5a21ad2f
TheAsel <a.osele@proton.me>
2023-10-13 15:57:11 +0200

    updated footer policy

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,8 +10,13 @@ import HeaderBar from 'components/HeaderBar.vue';
       <footer class="tw-relative tw-bottom-0 tw-inset-x-0 tw-text-center tw-py-1">
         <div class="tw-max-w-[85rem] tw-mx-auto tw-px-4 sm:tw-px-6 lg:tw-px-8">
           <p class="tw-text-sm dark:tw-text-white/50 tw-text-black/70">
-            This website is not published, endorsed, or specifically approved by Paizo Inc. We are
-            expressly prohibited from charging you to use or access this content.
+            BYBE uses trademarks and/or copyrights owned by Paizo Inc., used under Paizo's Community
+            Use Policy (<a href="https://paizo.com/communityuse" target="_blank"
+              >paizo.com/communityuse</a
+            >). We are expressly prohibited from charging you to use or access this content. BYBE is
+            not published, endorsed, or specifically approved by Paizo. For more information about
+            Paizo Inc. and Paizo products, visit
+            <a href="https://paizo.com" target="_blank">paizo.com</a>.
           </p>
         </div>
       </footer>
