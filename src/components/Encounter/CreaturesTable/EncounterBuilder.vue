d85e7cb76ac15b322ca15e9cbfe7e25f76fec33e
TheAsel <theasel99@gmail.com>
2023-10-19 17:07:49 +0200

    cache control, temp generator settings

--- src/components/Encounter/CreaturesTable/EncounterBuilder.vue
+++ src/components/Encounter/CreaturesTable/EncounterBuilder.vue
@@ -15,15 +15,33 @@ const encounter = encounterStore();
 
 const dialog = ref(false);
 
+const challenges = ['Trivial', 'Low', 'Moderate', 'Severe', 'Extreme', 'Impossible'];
+
 const family = ref();
 const alignment = ref();
 const size = ref();
 const rarity = ref();
+const challenge = ref();
 
-const challenges = ['Trivial', 'Low', 'Moderate', 'Severe', 'Extreme', 'Impossible'];
-const filterChallenge = ref();
+const tmpFilters = ref({
+  family: family.value,
+  alignment: alignment.value,
+  size: size.value,
+  rarity: rarity.value,
+  challenge: challenge.value
+});
+
+const restoreSettings = () => {
+  dialog.value = true;
+  tmpFilters.value.family = family.value;
+  tmpFilters.value.alignment = alignment.value;
+  tmpFilters.value.rarity = rarity.value;
+  tmpFilters.value.size = size.value;
+  tmpFilters.value.challenge = challenge.value;
+};
 
 const generateEncounter = async () => {
+  saveChanges();
   const partyLevels = party.getParty;
   const post = {
     party_levels: partyLevels
@@ -31,11 +49,11 @@ const generateEncounter = async () => {
   try {
     const randomEncounter = await encounterGenerator(
       post,
-      family.value,
-      alignment.value,
-      size.value,
-      rarity.value,
-      filterChallenge.value
+      tmpFilters.value.family,
+      tmpFilters.value.alignment,
+      tmpFilters.value.size,
+      tmpFilters.value.rarity,
+      tmpFilters.value.challenge
     );
     if (typeof randomEncounter != 'undefined') {
       if (randomEncounter.count > 0 && randomEncounter.results) {
@@ -59,15 +77,23 @@ const generateEncounter = async () => {
   }
 };
 
+const saveChanges = () => {
+  family.value = tmpFilters.value.family;
+  alignment.value = tmpFilters.value.alignment;
+  rarity.value = tmpFilters.value.rarity;
+  size.value = tmpFilters.value.size;
+  challenge.value = tmpFilters.value.challenge;
+};
+
 defineExpose({ generateEncounter });
 </script>
 
 <template>
-  <q-btn push label="Encounter Builder" @click="dialog = true" id="v-step-2" />
-  <q-dialog v-model="dialog" aria-label="Encounter builder">
+  <q-btn push label="Generator Settings" @click="restoreSettings" id="v-step-2" />
+  <q-dialog v-model="dialog" aria-label="Generator Settings">
     <q-card flat bordered>
       <q-card-section class="row items-center">
-        <div class="text-h6 tw-mr-4">Encounter Builder</div>
+        <div class="text-h6 tw-mr-4">Generator Settings</div>
         <q-space />
         <q-btn
           :icon="biXLg"
@@ -91,7 +117,7 @@ defineExpose({ generateEncounter });
             clearable
             :clear-icon="matCancel"
             options-dense
-            v-model="family"
+            v-model="tmpFilters.family"
             :options="Object.freeze(filters.getFilters.family)"
             label="Family"
             :dropdown-icon="matArrowDropDown"
@@ -103,7 +129,7 @@ defineExpose({ generateEncounter });
             clearable
             :clear-icon="matCancel"
             options-dense
-            v-model="alignment"
+            v-model="tmpFilters.alignment"
             :options="Object.freeze(filters.getFilters.alignment)"
             label="Alignment"
             :dropdown-icon="matArrowDropDown"
@@ -115,7 +141,7 @@ defineExpose({ generateEncounter });
             clearable
             :clear-icon="matCancel"
             options-dense
-            v-model="size"
+            v-model="tmpFilters.size"
             :options="Object.freeze(filters.getFilters.size)"
             label="Size"
             :dropdown-icon="matArrowDropDown"
@@ -127,7 +153,7 @@ defineExpose({ generateEncounter });
             clearable
             :clear-icon="matCancel"
             options-dense
-            v-model="rarity"
+            v-model="tmpFilters.rarity"
             :options="Object.freeze(filters.getFilters.rarity)"
             label="Rarity"
             :dropdown-icon="matArrowDropDown"
@@ -139,8 +165,8 @@ defineExpose({ generateEncounter });
             clearable
             :clear-icon="matCancel"
             options-dense
-            v-model="filterChallenge"
-            :options="challenges"
+            v-model="tmpFilters.challenge"
+            :options="Object.freeze(challenges)"
             label="Challenge"
             :dropdown-icon="matArrowDropDown"
           />
@@ -148,14 +174,24 @@ defineExpose({ generateEncounter });
       </q-card-section>
       <q-separator />
       <q-card-actions>
-        <q-btn
-          unelevated
-          label="Generate Encounter"
-          type="submit"
-          class="full-width tw-text-blue-600 dark:tw-text-blue-400"
-          v-close-popup
-          @click="generateEncounter"
-        />
+        <q-btn-group>
+          <q-btn
+            unelevated
+            label="Save changes"
+            type="submit"
+            class="full-width tw-text-blue-600 dark:tw-text-blue-400"
+            v-close-popup
+            @click="saveChanges"
+          />
+          <q-btn
+            unelevated
+            label="Generate Encounter"
+            type="submit"
+            class="full-width tw-text-blue-600 dark:tw-text-blue-400"
+            v-close-popup
+            @click="generateEncounter"
+          />
+        </q-btn-group>
       </q-card-actions>
     </q-card>
   </q-dialog>
