424db4ab901a25f7838790c5a9add0ed2c84b90f
TheAsel <theasel99@gmail.com>
2024-02-20 03:30:55 +0100

    switched to vuejs-tour
    vue3-tour is broken for new versions of vue

--- src/boot/globals.ts
+++ src/boot/globals.ts
@@ -1,14 +1,18 @@
+import { ref } from 'vue';
 import { boot } from 'quasar/wrappers';
-import Vue3Tour from 'vue3-tour';
+import VueJsTour from '@globalhive/vuejs-tour';
 import { createHead } from '@unhead/vue';
 
+const head = createHead();
 const backendUrl = 'https://backbybe.fly.dev';
+const tour = ref();
 
 export default boot(({ app }) => {
-  app.use(Vue3Tour);
-  app.use(createHead);
+  app.use(VueJsTour);
+  app.use(head);
 
   app.config.globalProperties.$BACKEND_URL = backendUrl;
+  app.config.globalProperties.$TOUR = tour;
 });
 
-export { backendUrl };
+export { backendUrl, tour };
