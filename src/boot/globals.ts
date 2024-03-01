7576007a70148cfe6b749e659891db5e027b4046
TheAsel <a.osele@proton.me>
2024-03-01 19:07:30 +0100

    Revert "switched to vuejs-tour"
    what was breaking was some interaction of bun/node/quasar (probably), not vue3-tour. switching to npm for now, sorry bun

--- src/boot/globals.ts
+++ src/boot/globals.ts
@@ -1,18 +1,15 @@
-import { ref } from 'vue';
 import { boot } from 'quasar/wrappers';
-import VueJsTour from '@globalhive/vuejs-tour';
+import Vue3Tour from 'vue3-tour';
 import { createHead } from '@unhead/vue';
 
 const head = createHead();
 const backendUrl = 'https://backbybe.fly.dev';
-const tour = ref();
 
 export default boot(({ app }) => {
-  app.use(VueJsTour);
+  app.use(Vue3Tour);
   app.use(head);
 
   app.config.globalProperties.$BACKEND_URL = backendUrl;
-  app.config.globalProperties.$TOUR = tour;
 });
 
-export { backendUrl, tour };
+export { backendUrl };
