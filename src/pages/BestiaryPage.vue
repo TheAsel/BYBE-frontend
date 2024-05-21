8af55a566c64a46cc981571603449ec0af464e5e
TheAsel <a.osele@proton.me>
2024-05-21 23:13:44 +0200

    ✨ Creature sheet: name, tags, perception

--- src/pages/BestiaryPage.vue
+++ src/pages/BestiaryPage.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useHead } from '@unhead/vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useQuasar } from 'quasar';
@@ -7,8 +8,10 @@ import type { creature } from 'src/types/creature';
 import { requestCreatureId } from 'src/utils/api-calls';
 import { isNull } from 'lodash';
 
+const title = ref('Creature Sheet - BYBE')
+
 useHead({
-  title: 'Creature Sheet - BYBE',
+  title: title,
   link: [
     {
       rel: 'canonical',
@@ -35,6 +38,8 @@ try {
         icon: matPriorityHigh
       });
       router.push({ name: 'encounter' });
+    } else {
+      title.value = creatureData!.core_data.essential.name + ' - BYBE'
     }
   } else {
     console.error('Invalid creature ID');
@@ -49,18 +54,85 @@ try {
 } catch (error) {
   console.error(error);
 }
+
+const addPlus = (value: number) => {
+  if (value > 0) {
+    return '+' + value
+  } else {
+    return value
+  }
+}
+
+const perceptionString = () => {
+  const perception = creatureData!.extra_data!.perception
+  const senses = creatureData!.extra_data!.senses
+  var finalString = addPlus(perception)
+  if (senses.length > 0) {
+    finalString = finalString + '; '
+    senses.forEach((sense) => {
+      return creatureData!.extra_data!.actions.forEach((action) => {
+        if (action.slug === sense) {
+          finalString = finalString + action.name.toLowerCase() + ', '
+        }
+      })
+    })
+    if (creatureData!.extra_data!.perception_detail) {
+      finalString = finalString + creatureData!.extra_data!.perception_detail
+    } else {
+      finalString = finalString.substring(0, finalString.length - 2);
+    }
+  }
+  return finalString
+}
+
 </script>
 
 <template>
-  <q-page class="tw-flex row items-center justify-evenly">
-    <div class="tw-text-center tw-py-10 tw-px-4 sm:tw-px-6 lg:tw-px-8">
-      <h1
-        class="tw-block tw-text-base tw-font-bold tw-text-gray-800 dark:tw-text-white sm:tw-text-lg"
-      >
-        {{ creatureData?.core_data.essential.name.toUpperCase() }}
-        {{ creatureData?.core_data.essential.cr_type.toUpperCase() }}
-        {{ creatureData?.core_data.essential.level }}
-      </h1>
+  <q-page class="tw-px-96 tw-py-4 items-center justify-evenly">
+    <div class="tw-flex tw-flex-row tw-font-bold tw-text-lg tw-text-gray-800 dark:tw-text-white">
+      <div class="tw-flex-none">
+        {{ creatureData!.core_data.essential.name.toUpperCase() }}
+      </div>
+      <div class="tw-flex-grow" />
+      <div class="tw-flex-none">
+        {{ creatureData!.core_data.essential.cr_type.toUpperCase() }}
+        {{ creatureData!.core_data.essential.level }}
+      </div>
+    </div>
+    <q-separator style="height: 2px;" />
+    <div class="tw-flex tw-flex-rows tw-font-bold tw-text-xs tw-text-white">
+      <div v-if="creatureData!.core_data.essential.rarity === 'Common'" />
+      <div v-else-if="creatureData!.core_data.essential.rarity === 'Uncommon'"
+        class="tw-bg-[#c45500] tw-border-2 tw-border-[#d8c483] tw-my-1 tw-p-1">
+        {{ creatureData!.core_data.essential.rarity.toUpperCase() }}
+      </div>
+      <div v-else-if="creatureData!.core_data.essential.rarity === 'Rare'"
+        class="tw-bg-[#0c1466] tw-border-2 tw-border-[#d8c483] tw-my-1 tw-p-1">
+        {{ creatureData!.core_data.essential.rarity.toUpperCase() }}
+      </div>
+      <div v-else-if="creatureData!.core_data.essential.rarity === 'Unique'"
+        class="tw-bg-[#800080] tw-border-2 tw-border-[#d8c483] tw-my-1 tw-p-1">
+        {{ creatureData!.core_data.essential.rarity.toUpperCase() }}
+      </div>
+      <div class="tw-bg-[#4287f5] tw-border-2 tw-border-[#d8c483] tw-my-1 tw-p-1">
+        {{ creatureData!.core_data.essential.alignment.toUpperCase() }}
+      </div>
+      <div class="tw-bg-[#478c42] tw-border-2 tw-border-[#d8c483] tw-my-1 tw-p-1">
+        {{ creatureData!.core_data.essential.size.toUpperCase() }}
+      </div>
+      <div v-for="item in creatureData!.core_data.traits" :key="item"
+        class="tw-bg-[#522e2c] tw-border-2 tw-border-[#d8c483] tw-my-1 tw-p-1">
+        {{ item.toUpperCase() }}
+      </div>
+    </div>
+    <div class="tw-flex tw-flex-row tw-text-sm tw-text-gray-800 dark:tw-text-white">
+      <b>Source</b> &nbsp; <i class="tw-text-[#60a5fa]"> {{ creatureData!.core_data.essential.source }} </i>
+    </div>
+    <div class="tw-flex tw-flex-row tw-text-sm tw-text-gray-800 dark:tw-text-white">
+      <b>Perception</b> &nbsp;
+      <div>
+        {{ perceptionString() }}
+      </div>
     </div>
   </q-page>
 </template>
