eb5848859999f264c5b66f3590ac4ca794ad3f3c
TheAsel <a.osele@proton.me>
2024-05-26 19:39:15 +0200

    ♻️ Broke down cleanDescription in smaller functions

--- src/pages/BestiaryPage.vue
+++ src/pages/BestiaryPage.vue
@@ -107,99 +107,126 @@ const variantStyle = (value: number | undefined) => {
   return value;
 };
 
-const cleanDescription = (description: string) => {
-  let finalString = '';
-  const cleanRegex = /<\/?(?:p)?(?:li)?(?:ul)?>|<hr ?\/>|@Localize\[.+\]/g;
+const cleanCompendium = (description: string) => {
   const compendiumRegex =
     /@UUID\[Compendium\.([\w\-\s]*)\.([\w\-\s]*)\.([\w\-\s]*)\.([\w\-\s'()+]*)\](?:{([\w\s'+]*)})?/g;
-  const effectRegex =
-    /@UUID\[Compendium\.([\w\-\s]*)\.([\w\-\s]*)\.([\w\-\s]*)\.([\w\-\s]*): ([\w\s\-'()]*)\](?:{([\w\s']*)})?/g;
-  const damageRegex =
-    /@Damage\[\(?([\w+]*)\)?\[(\w*),?(\w+)?\]?,?(\w+)?\[?(\w+)?\]?(?:[\w|:,[\]-]+)?\](?:\{([\w\s,+]*)\})?/g;
-  const templateRegex =
-    /@Template\[type:(\w*)\|distance:(\d*)\|?(?:traits:([\w\-,]*))?\](?:{([\w\s-]*)})?/g;
-  const checkRegex =
-    /@Check\[type:(\w*)(?:[\w\s\-|]*dc:([\w\s,:+@.()]*))?(?:[\w\s\-,:|()]*basic:(\w*))?[\w\s\-,:|()]*\](?:{([\w\s'+()]*)})?/g;
-  const rollRegex =
-    /\[\[\/b?r \(?{?(\d\*?\d*d?[\d\s\-+]*\d*),?\d*}?\)?[\w\s]*\[?#?[\w\s,]*\]\]\]?(?:{([\w\s\-+;]*)})?/g;
 
-  finalString = description.replace(cleanRegex, '');
-  finalString = finalString.replace(effectRegex, '');
-
-  const compendium = finalString.matchAll(compendiumRegex);
+  const compendium = description.matchAll(compendiumRegex);
   for (const i of compendium) {
     if (i) {
       if (i[5]) {
-        finalString = finalString.replace(i[0], i[5].toLowerCase());
+        description = description.replace(i[0], i[5].toLowerCase());
       } else {
-        finalString = finalString.replace(i[0], i[4].toLowerCase());
+        description = description.replace(i[0], i[4].toLowerCase());
       }
     }
   }
+  return description;
+};
 
-  const damage = finalString.matchAll(damageRegex);
+const cleanDamage = (description: string) => {
+  const damageRegex =
+    /@Damage\[\(?([\w+]*)\)?\[(\w*),?(\w+)?\]?,?(\w+)?\[?(\w+)?\]?(?:[\w|:,[\]-]+)?\](?:\{([\w\s,+]*)\})?/g;
+
+  const damage = description.matchAll(damageRegex);
   for (const i of damage) {
     if (i) {
       if (i[6]) {
-        finalString = finalString.replace(i[0], i[6]);
+        description = description.replace(i[0], i[6]);
       } else if (i[4]) {
-        finalString = finalString.replace(i[0], i[1] + ' ' + i[2] + ' plus ' + i[4] + ' ' + i[5]);
+        description = description.replace(i[0], i[1] + ' ' + i[2] + ' plus ' + i[4] + ' ' + i[5]);
       } else if (i[3]) {
-        finalString = finalString.replace(i[0], i[1] + ' ' + i[2] + ' ' + i[3]);
+        description = description.replace(i[0], i[1] + ' ' + i[2] + ' ' + i[3]);
       } else {
-        finalString = finalString.replace(i[0], i[1] + ' ' + i[2]);
+        description = description.replace(i[0], i[1] + ' ' + i[2]);
       }
     }
   }
+  return description;
+};
 
-  const template = finalString.matchAll(templateRegex);
+const cleanTemplate = (description: string) => {
+  const templateRegex =
+    /@Template\[type:(\w*)\|distance:(\d*)\|?(?:traits:([\w\-,]*))?\](?:{([\w\s-]*)})?/g;
+
+  const template = description.matchAll(templateRegex);
   for (const i of template) {
     if (i) {
-      finalString = finalString.replace(i[0], i[2] + '-foot ' + i[1]);
+      description = description.replace(i[0], i[2] + '-foot ' + i[1]);
     }
   }
+  return description;
+};
 
-  const save = finalString.matchAll(checkRegex);
+const cleanSave = (description: string) => {
+  const checkRegex =
+    /@Check\[type:(\w*)(?:[\w\s\-|]*dc:([\w\s,:+@.()]*))?(?:[\w\s\-,:|()]*basic:(\w*))?[\w\s\-,:|()]*\](?:{([\w\s'+()]*)})?/g;
+
+  const save = description.matchAll(checkRegex);
   for (const i of save) {
     if (i) {
       if (i[2] && Number(i[2])) {
         if (i[3] === 'true') {
           if (i[4]) {
-            finalString = finalString.replace(
+            description = description.replace(
               i[0],
               'DC ' + i[2] + ' basic ' + _.upperFirst(i[1]) + ' ' + i[4]
             );
           } else {
-            finalString = finalString.replace(i[0], 'DC ' + i[2] + ' basic ' + _.upperFirst(i[1]));
+            description = description.replace(i[0], 'DC ' + i[2] + ' basic ' + _.upperFirst(i[1]));
           }
         } else if (i[4]) {
-          finalString = finalString.replace(
+          description = description.replace(
             i[0],
             'DC ' + i[2] + ' ' + _.upperFirst(i[1]) + ' ' + i[4]
           );
         } else {
-          finalString = finalString.replace(i[0], 'DC ' + i[2] + ' ' + _.upperFirst(i[1]));
+          description = description.replace(i[0], 'DC ' + i[2] + ' ' + _.upperFirst(i[1]));
         }
       } else if (i[4]) {
-        finalString = finalString.replace(i[0], _.upperFirst(i[1]) + ' ' + i[4]);
+        description = description.replace(i[0], _.upperFirst(i[1]) + ' ' + i[4]);
       } else {
-        finalString = finalString.replace(i[0], _.upperFirst(i[1]));
+        description = description.replace(i[0], _.upperFirst(i[1]));
       }
     }
   }
+  return description;
+};
 
-  const roll = finalString.matchAll(rollRegex);
+const cleanRoll = (description: string) => {
+  const rollRegex =
+    /\[\[\/b?r \(?{?(\d\*?\d*d?[\d\s\-+]*\d*),?\d*}?\)?[\w\s]*\[?#?[\w\s,]*\]\]\]?(?:{([\w\s\-+;]*)})?/g;
+
+  const roll = description.matchAll(rollRegex);
   for (const i of roll) {
     if (i) {
       if (i[2]) {
-        finalString = finalString.replace(i[0], i[2]);
+        description = description.replace(i[0], i[2]);
       } else {
-        finalString = finalString.replace(i[0], i[1]);
+        description = description.replace(i[0], i[1]);
       }
     }
   }
+  return description;
+};
 
-  return finalString;
+const cleanDescription = (description: string) => {
+  const cleanRegex = /<\/?(?:p)?(?:li)?(?:ul)?>|<hr ?\/>|@Localize\[.+\]/g;
+  const effectRegex =
+    /@UUID\[Compendium\.([\w\-\s]*)\.([\w\-\s]*)\.([\w\-\s]*)\.([\w\-\s]*): ([\w\s\-'()]*)\](?:{([\w\s']*)})?/g;
+
+  let finalString = description.replace(cleanRegex, '');
+  finalString = finalString.replace(effectRegex, '');
+
+  finalString = cleanCompendium(finalString);
+
+  finalString = cleanDamage(finalString);
+
+  finalString = cleanTemplate(finalString);
+
+  finalString = cleanSave(finalString);
+
+  return cleanRoll(finalString);
 };
 
 const nameString = computed(() => {
