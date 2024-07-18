2dc398663c059fb6f6c13fe3a499d1735a537d55
TheAsel <a.osele@proton.me>
2024-07-18 16:02:58 +0200

    🚚 Switch to prod server

--- src/boot/globals.ts
+++ src/boot/globals.ts
@@ -3,7 +3,7 @@ import Vue3Tour from 'vue3-tour';
 import { createHead } from '@unhead/vue';
 
 const head = createHead();
-const backendUrl = 'https://dev-test-backbybe.fly.dev';
+const backendUrl = 'https://backbybe.fly.dev';
 
 export default boot(({ app }) => {
   app.use(Vue3Tour);
