01a657804e922175289ab6b2a5db27f5dc08fc73
TheAsel <a.osele@proton.me>
2026-02-19 17:48:33 +0100

    🔒️ Fix kofi widget hash

--- src/components/SettingsMenu.vue
+++ src/components/SettingsMenu.vue
@@ -79,7 +79,7 @@ const loadKofiWidget = () => {
     const kofiWidget = document.createElement('script');
     kofiWidget.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
     kofiWidget.integrity =
-      'sha512-M5C7x3flCdJVRDM/E9jAWOtukG3A+9K2vGYYsi5D8fr49cvowu+aPOS47S/gzuUFfzMy2OJ7IqfBB+NE9kmnEw== sha512-Qq9Wa8l8WJo/UHkeTG+uo0cuHH8TgQ0yN3TXdRSUxhwIpgNRyqeqeCen+JED6L/772FJbduOYvZ31yhxYHegJg==';
+      'sha512-lcOFkHZZIXe4UQdsL95YQNPTGWRzMBlh4CU8XRt0E1TLqeoJG5i7WgGZ0cSPB1Uua5EF4gy6UibwS8kBuHU8hg==';
     kofiWidget.crossOrigin = 'anonymous';
     kofiWidget.async = true;
     kofiWidget.onload = () => {
