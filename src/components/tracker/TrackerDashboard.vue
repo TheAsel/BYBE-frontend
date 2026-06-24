1307db61322473642202ebba52d39923cb70b129
Osele Antonio <a.osele@proton.me>
2026-06-24 17:22:04 +0200

    ✨ Initiative in dashboard, common d20 icon svg

--- src/components/tracker/TrackerDashboard.vue
+++ src/components/tracker/TrackerDashboard.vue
@@ -11,6 +11,8 @@ import {
 import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 
+import DiceIcon from "@/components/generic/DiceIcon.vue";
+
 const router = useRouter();
 
 const settings_store = settingsStore();
@@ -26,8 +28,8 @@ const validateNumber = (newValue: unknown): number => {
   const val = Number(newValue);
   if (Number.isNaN(val) || val < 0) {
     return 0;
-  } else if (val > 9999) {
-    return 9999;
+  } else if (val > 999) {
+    return 999;
   }
   return Math.round(val);
 };
@@ -46,6 +48,14 @@ const changeHealth = (newValue: unknown): void => {
     } else {
       detailedElement.value.health = sum;
     }
+  }
+};
+
+const rollInitiative = (index: number): void => {
+  const rollDice = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+  if (tracker_store.trackerList.list[index]) {
+    tracker_store.trackerList.list[index]!.initiative =
+      rollDice + tracker_store.trackerList.list[index].perception;
   }
 };
 </script>
@@ -70,121 +80,149 @@ const changeHealth = (newValue: unknown): void => {
       <q-scroll-area class="tw:h-full">
         <div
           v-if="detailedElement"
-          class="tw:h-full tw:m-6 tw:text-gray-800 tw:dark:text-white"
+          class="tw:h-full tw:m-4 tw:text-gray-800 tw:dark:text-white"
         >
-          <div v-if="detailedElement.is_player" class="tw:flex tw:my-3">
-            <h1
-              :class="
-                getGameFontSize(settings_store.game) +
-                ' tw:mr-4 tw:leading-8 tw:my-auto tw:break-all'
-              "
+          <div class="tw:flex">
+            <span v-if="detailedElement.is_player" class="tw:flex">
+              <h1
+                :class="
+                  getGameFontSize(settings_store.game) +
+                  ' tw:mr-4 tw:leading-8 tw:my-auto tw:break-all'
+                "
+                :style="
+                  'font-family: ' +
+                  getGameFont(settings_store.game) +
+                  ', sans-serif; font-variant-caps: small-caps'
+                "
+              >
+                {{ detailedElement.element }}
+              </h1>
+            </span>
+            <span
+              v-else
+              class="tw:flex"
               :style="
                 'font-family: ' +
-                getGameFont(settings_store.game) +
+                getGameFont(
+                  !detailedElement.is_player
+                    ? detailedElement.element.game
+                    : settings_store.game
+                ) +
                 ', sans-serif; font-variant-caps: small-caps'
               "
             >
-              {{ detailedElement.element }}
-            </h1>
-          </div>
-          <div
-            v-else
-            class="tw:flex tw:my-3"
-            :style="
-              'font-family: ' +
-              getGameFont(
-                !detailedElement.is_player
-                  ? detailedElement.element.game
-                  : settings_store.game
-              ) +
-              ', sans-serif; font-variant-caps: small-caps'
-            "
-          >
-            <q-btn
-              :icon="biBoxArrowUpRight"
-              flat
-              round
-              dense
-              size="sm"
-              class="tw:px-2! only-screen encounter-page-element"
-              aria-label="Open creature sheet"
-              @click="
-                openSheet(
-                  router,
-                  detailedElement.element.is_hazard ? 'hazard' : 'bestiary',
-                  detailedElement.element.game ?? settings_store.game,
-                  detailedElement.element.id ?? 0,
-                  !detailedElement.element.is_hazard
-                    ? detailedElement.element.variant
-                    : null
-                )
-              "
-            >
-              <q-tooltip
-                class="text-caption tw:bg-gray-700! tw:text-gray-200! tw:rounded-md tw:shadow-sm tw:dark:bg-slate-700!"
-                anchor="top middle"
-                self="bottom middle"
-              >
-                {{
-                  "Open " +
-                  (detailedElement.element.is_hazard ? "hazard" : "creature") +
-                  " sheet"
-                }}
-              </q-tooltip>
-            </q-btn>
-            <a
-              v-if="
-                !detailedElement.element.is_hazard &&
-                detailedElement.element.archive_link
-              "
-              class="tw:my-auto"
-              :href="
-                detailedElement.element.archive_link +
-                '&Weak=' +
-                (detailedElement.element.variant === 'Weak') +
-                '&Elite=' +
-                (detailedElement.element.variant === 'Elite')
-              "
-              target="_blank"
-              rel="noopener"
-            >
-              <h1
-                :class="
-                  getGameFontSize(
-                    detailedElement.element.game ?? settings_store.game
-                  ) +
-                  ' tw:mr-4 tw:leading-8 tw:text-blue-600 tw:decoration-2 tw:hover:underline tw:dark:text-blue-400'
+              <q-btn
+                :icon="biBoxArrowUpRight"
+                flat
+                round
+                dense
+                size="sm"
+                class="tw:px-2! only-screen encounter-page-element"
+                aria-label="Open creature sheet"
+                @click="
+                  openSheet(
+                    router,
+                    detailedElement.element.is_hazard ? 'hazard' : 'bestiary',
+                    detailedElement.element.game ?? settings_store.game,
+                    detailedElement.element.id ?? 0,
+                    !detailedElement.element.is_hazard
+                      ? detailedElement.element.variant
+                      : null
+                  )
                 "
               >
-                <span v-if="detailedElement.element.variant === 'Weak'"
-                  >Weak
-                </span>
-                <span v-else-if="detailedElement.element.variant === 'Elite'"
-                  >Elite
-                </span>
-                {{ detailedElement.element.name }}
-              </h1>
-            </a>
-            <a
-              v-else-if="
-                detailedElement.element.game === 'sf' ||
-                detailedElement.element.is_hazard
-              "
-              class="tw:my-auto"
-              :href="
-                'https://2e.aonsrd.com/search?q=' +
-                encodeURIComponent(detailedElement.element.name) +
-                ' type%3A(creature)&type=eqs'
-              "
-              target="_blank"
-              rel="noopener"
-            >
+                <q-tooltip
+                  class="text-caption tw:bg-gray-700! tw:text-gray-200! tw:rounded-md tw:shadow-sm tw:dark:bg-slate-700!"
+                  anchor="top middle"
+                  self="bottom middle"
+                >
+                  {{
+                    "Open " +
+                    (detailedElement.element.is_hazard
+                      ? "hazard"
+                      : "creature") +
+                    " sheet"
+                  }}
+                </q-tooltip>
+              </q-btn>
+              <a
+                v-if="
+                  !detailedElement.element.is_hazard &&
+                  detailedElement.element.archive_link
+                "
+                class="tw:my-auto"
+                :href="
+                  detailedElement.element.archive_link +
+                  '&Weak=' +
+                  (detailedElement.element.variant === 'Weak') +
+                  '&Elite=' +
+                  (detailedElement.element.variant === 'Elite')
+                "
+                target="_blank"
+                rel="noopener"
+              >
+                <h1
+                  :class="
+                    getGameFontSize(
+                      detailedElement.element.game ?? settings_store.game
+                    ) +
+                    ' tw:mr-4 tw:leading-8 tw:text-blue-600 tw:decoration-2 tw:hover:underline tw:dark:text-blue-400'
+                  "
+                >
+                  <span v-if="detailedElement.element.variant === 'Weak'"
+                    >Weak
+                  </span>
+                  <span v-else-if="detailedElement.element.variant === 'Elite'"
+                    >Elite
+                  </span>
+                  {{ detailedElement.element.name }}
+                </h1>
+              </a>
+              <a
+                v-else-if="
+                  detailedElement.element.game === 'sf' ||
+                  detailedElement.element.is_hazard
+                "
+                class="tw:my-auto"
+                :href="
+                  'https://2e.aonsrd.com/search?q=' +
+                  encodeURIComponent(detailedElement.element.name) +
+                  ' type%3A(creature)&type=eqs'
+                "
+                target="_blank"
+                rel="noopener"
+              >
+                <h1
+                  :class="
+                    getGameFontSize(
+                      detailedElement.element.game ?? settings_store.game
+                    ) +
+                    ' tw:mr-4 tw:leading-8 tw:text-blue-600 tw:decoration-2 tw:hover:underline tw:dark:text-blue-400'
+                  "
+                >
+                  <span
+                    v-if="
+                      !detailedElement.element.is_hazard &&
+                      detailedElement.element.variant === 'Weak'
+                    "
+                    >Weak
+                  </span>
+                  <span
+                    v-else-if="
+                      !detailedElement.element.is_hazard &&
+                      detailedElement.element.variant === 'Elite'
+                    "
+                    >Elite
+                  </span>
+                  {{ detailedElement.element.name }}
+                </h1>
+              </a>
               <h1
+                v-else
                 :class="
                   getGameFontSize(
                     detailedElement.element.game ?? settings_store.game
-                  ) +
-                  ' tw:mr-4 tw:leading-8 tw:text-blue-600 tw:decoration-2 tw:hover:underline tw:dark:text-blue-400'
+                  ) + ' tw:mr-4 tw:leading-8 tw:my-auto'
                 "
               >
                 <span
@@ -203,31 +241,30 @@ const changeHealth = (newValue: unknown): void => {
                 </span>
                 {{ detailedElement.element.name }}
               </h1>
-            </a>
-            <h1
-              v-else
-              :class="
-                getGameFontSize(
-                  detailedElement.element.game ?? settings_store.game
-                ) + ' tw:mr-4 tw:leading-8 tw:my-auto'
+            </span>
+            <q-space />
+            <q-btn
+              color="primary tw:my-auto!"
+              round
+              unelevated
+              aria-label="Roll initiative"
+              @click="
+                rollInitiative(tracker_store.trackerList.detail_index);
+                tracker_store.sortList();
               "
             >
-              <span
-                v-if="
-                  !detailedElement.element.is_hazard &&
-                  detailedElement.element.variant === 'Weak'
-                "
-                >Weak
-              </span>
-              <span
-                v-else-if="
-                  !detailedElement.element.is_hazard &&
-                  detailedElement.element.variant === 'Elite'
-                "
-                >Elite
-              </span>
-              {{ detailedElement.element.name }}
-            </h1>
+              <DiceIcon v-if="detailedElement.initiative === null" />
+              <b v-else class="tw:text-xl">
+                {{ detailedElement.initiative }}
+              </b>
+              <q-tooltip
+                class="text-caption tw:bg-gray-700! tw:text-gray-200! tw:rounded-md tw:shadow-sm tw:dark:bg-slate-700!"
+                anchor="top middle"
+                self="bottom middle"
+              >
+                Roll initiative
+              </q-tooltip>
+            </q-btn>
           </div>
           <q-separator class="tw:my-2!" style="height: 2px" />
           <div
@@ -243,7 +280,7 @@ const changeHealth = (newValue: unknown): void => {
                   :model-value="detailedElement.health"
                   dense
                   borderless
-                  class="tw:max-w-12! tw:max-h-8! tw:text-xl! tw:mr-1"
+                  class="tw:max-w-8! tw:max-h-8! tw:text-xl! tw:mr-1"
                   input-class="tw:text-xl! tw:font-bold! tw:text-end"
                   type="number"
                   @update:model-value="
@@ -256,7 +293,7 @@ const changeHealth = (newValue: unknown): void => {
                   dense
                   borderless
                   prefix="/"
-                  class="tw:max-w-20! tw:max-h-8! tw:text-xl!"
+                  class="tw:max-w-18! tw:max-h-8! tw:text-xl!"
                   input-class="tw:text-xl! tw:font-bold!"
                   type="number"
                   @update:model-value="
